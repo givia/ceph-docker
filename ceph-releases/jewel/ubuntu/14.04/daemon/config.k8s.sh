777a43d491faff85d383e7a8e38c2ed72403a7e1
Ali Javadi <ali.djavadi@gmail.com>
2016-10-18 13:48:43 +0330

    add ceph-common to Dockerfile

--- ceph-releases/jewel/ubuntu/14.04/daemon/config.k8s.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/config.k8s.sh
@@ -11,7 +11,7 @@ function get_mon_config {
   FSID=$(ceph-conf --lookup fsid -c /etc/ceph/ceph.conf)
 
   # Get the ceph mon pods (name and IP) from the Kubernetes API. Formatted as a set of monmap params
-  MONMAP_ADD=$(kubectl get pods --namespace=${CLUSTER} -l daemon=mon -o template --template="{{range .items}}{{if .status.podIP }}--add {{.metadata.name}} {{ .status.podIP }}{{end}}{{end}}")
+  MONMAP_ADD=$(LOCALIP=`hostname -I | cut -d' ' -f1` kubectl --namespace ceph get pods -l daemon=mon -o template --template='{{range .items}}{{if .status.podIP}} --add {{.metadata.name}} {{ if ne .status.podIP "${LOCALIP}" }}{{ .status.podIP }}{{else}}127.0.0.1{{end}}{{end}}{{end}}')
 
   # Create a monmap with the Pod Names and IP
   monmaptool --create ${MONMAP_ADD} --fsid ${FSID} /etc/ceph/monmap-${CLUSTER}
