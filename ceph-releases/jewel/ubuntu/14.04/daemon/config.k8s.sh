c7a106eb052289a1a7e2d29061cfc94d243e9903
Ali Javadi <ali.djavadi@gmail.com>
2016-10-25 16:03:58 +0330

    dns and reverse dns

--- ceph-releases/jewel/ubuntu/14.04/daemon/config.k8s.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/config.k8s.sh
@@ -7,6 +7,23 @@ function get_admin_key {
 }
 
 function get_mon_config {
+
+  HOSTIP1=$(hostname -I | cut -d'.' -f1 | cut -d ' ' -f1)
+  HOSTIP2=$(hostname -I | cut -d'.' -f2 | cut -d ' ' -f1)
+  HOSTIP3=$(hostname -I | cut -d'.' -f3 | cut -d ' ' -f1)
+  HOSTIP4=$(hostname -I | cut -d'.' -f4 | cut -d ' ' -f1)
+
+  i=0
+  INSERTED="0"
+  while [ ${INSERTED} -lt 1 ]; do
+    i=$(($i+1))
+    INSERTED=$(curl -L -XGET http://172.19.1.11:4001/v2/keys/skydns/cafecluster/svc/ceph/ceph-mon/x$i --head | grep "404" | wc -l)
+  done;
+
+  curl -XPUT http://172.19.1.11:4001/v2/keys/skydns/cafecluster/svc/ceph/ceph-mon/x$i -d value='{\"host\":\"`hostname -I | cut -d' ' -f1`\"}'
+  curl -XPUT http://172.19.1.11:4001/v2/keys/skydns/arpa/in-addr/${HOSTIP1}/${HOSTIP2}/${HOSTIP3}/${HOSTIP4} -d value='{\"host\":\"`hostname`\"}'
+
+
   # Get FSID from ceph.conf
   FSID=$(ceph-conf --lookup fsid -c /etc/ceph/ceph.conf)
 
